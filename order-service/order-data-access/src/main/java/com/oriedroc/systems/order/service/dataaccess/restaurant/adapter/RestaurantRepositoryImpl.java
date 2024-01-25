88d643978796b88261ea3aabf34b49c0884f73b0
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-01-25 17:27:23 -0300

    add restaurant data access

---
+++ order-service/order-data-access/src/main/java/com/oriedroc/systems/order/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.oriedroc.systems.order.service.dataaccess.restaurant.adapter;
+
+import com.oriedroc.systems.order.service.dataaccess.restaurant.entity.RestaurantEntity;
+import com.oriedroc.systems.order.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
+import com.oriedroc.systems.order.service.dataaccess.restaurant.repository.RestaurantJpaRepository;
+import com.oriedroc.systems.order.service.domain.entity.Restaurant;
+import com.oriedroc.systems.order.service.domain.ports.output.repository.RestaurantRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class RestaurantRepositoryImpl implements RestaurantRepository {
+
+    private final RestaurantJpaRepository restaurantJpaRepository;
+    private final RestaurantDataAccessMapper restaurantDataAccessMapper;
+
+    public RestaurantRepositoryImpl(RestaurantJpaRepository restaurantJpaRepository,
+                                    RestaurantDataAccessMapper restaurantDataAccessMapper) {
+        this.restaurantJpaRepository = restaurantJpaRepository;
+        this.restaurantDataAccessMapper = restaurantDataAccessMapper;
+    }
+
+    @Override
+    public Optional<Restaurant> findRestaurantInformation(Restaurant restaurant) {
+        List<UUID> restaurantProducts =
+                restaurantDataAccessMapper.restaurantToRestaurantProducts(restaurant);
+        Optional<List<RestaurantEntity>> restaurantEntities =  restaurantJpaRepository
+                .findByRestaurantIdAndProductIdIn(restaurant.getId().getValue(), restaurantProducts);
+        return restaurantEntities.map(restaurantDataAccessMapper::restaurantEntityToRestaurant);
+    }
+}
